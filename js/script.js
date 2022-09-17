b844cf7036284bb5e1ffbfd859633d820c160116
eliaquinn <eliaquin.j.araujo@gmail.com>
2022-09-17 11:13:01 -0400

    edit js files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 import Module from './module.js'
 
 const listDocs = document.querySelector('.recents ul')
-const btnSave = document.querySelector('.buttons button')
+const btnSave = document.getElementById('save')
+const btnCancel = document.getElementById('cancel')
 const authorDoc = document.getElementById('author')
 const titleDoc = document.getElementById('title')
 const contentDoc = document.getElementById('content')
+const newDoc = document.getElementById('new-doc')
+const docEditor = document.querySelector('.editor')
+const toolbar = document.querySelector('.toolbar')
 
 let mod = new Module()
+
 let elements = [
     authorDoc,
     titleDoc,
@@ -16,3 +21,8 @@ let elements = [
 
 mod.renderDataListDocs(listDocs)
 mod.savedDocument(btnSave, 'documents-saveds', elements)
+
+newDoc.addEventListener('click', () => {
+    toolbar.classList.add('active')
+    docEditor.classList.add('show')
+})
